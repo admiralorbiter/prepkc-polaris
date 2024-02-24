17eeb4c9a4f9d8bcaf623eec27da9ea3d02eb3c3
Jon Lane <admiralorbiter@gmail.com>
2024-02-24 12:25:47 -0600

    htmx filter complete

--- app/routes/views.py
+++ app/routes/views.py
@@ -1,15 +1,25 @@
-from flask import render_template
-from app import app, db
+from flask import render_template, request
+from app import app
 from app.models import Session
 
 # Home Page
 @app.route("/", methods=["GET"])
 def index():
-    sessions_data = Session.query.all()  # Fetch all session records from the database
+    status_filter = request.args.get('statusFilter')
+    if status_filter:
+        sessions_data = Session.query.filter_by(status=status_filter).all()
+    else:
+        sessions_data = Session.query.all()
     return render_template("sessions.html", sessions=sessions_data)
     # return render_template("index.html")
 
-@app.route("/sessions", methods=["GET"])
-def sessions():
-    sessions_data = Session.query.all()  # Fetch all session records from the database
-    return render_template("sessions.html", sessions=sessions_data)
+@app.route("/load-sessions-table", methods=["GET"])
+def load_sessions_table():
+    sessions_data = Session.query.all()
+    return render_template("session_table.html", sessions=sessions_data)
+
+@app.route("/filter-sessions", methods=["GET"])
+def filter_sessions():
+    status_filter = request.args.get('statusFilter')
+    sessions_data = Session.query.filter(Session.status.ilike(f"%{status_filter}%")).all() if status_filter else Session.query.all()
+    return render_template("session_table.html", sessions=sessions_data)
